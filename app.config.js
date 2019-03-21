c7cf8c4eebed67bd6dff8d8097770e6cb264a01a
huangjianhua <huangjianhua@ctrip.com>
2019-03-21 21:11:45 +0800

    [update] gh-pages

--- app.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  redisName: 'xxx',
-  xconfig: ['properties'],
-  Env: 'fws', // support dev, fat, uat, pro
-  // 'SOA.Timeout': 100 // [optional] soa全局的timeout，可以被单独调用的timeout覆盖
-  port: 3600, // 默认启动端口
-  middle: ''
-}
--- src/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  export interface Global {
-    configData: any
-  }
-}
