eafee2a4e9660a1018c173d5c5a867eedc7c8ecd
skyweaver213 <happyday0213@126.com>
2019-04-28 10:48:07 +0800

    [fixed] middleware

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as bodyParser from 'koa-bodyparser'
 import * as cookie from 'koa-cookie'
 import { ApolloServer } from 'apollo-server-koa'
 import { makeExecutableSchema } from 'graphql-tools' // SchemaDirectiveVisitor
-import { findMiddleAndRequire } from '../src/util/methods'
+import { findMiddleAndRequire } from './util/methods'
 const cwd = process.cwd()
 const yargs = require('yargs')
 const argv = yargs.argv
@@ -41,8 +41,10 @@ router = router && router.default
 // 如果项目根目录存在app.config 取项目的； 不存在取默认配置； 
 // 根目录app.config 优先级 > oneql默认配置
 const defaultConfigPath = '../app.config'
-const cwdPath = path.resolve(cwd, '../app.config')
+const cwdPath = path.resolve(cwd, 'app.config')
 const defaultPath = path.resolve(__dirname,  defaultConfigPath)
+
+console.log('cwdPath ', cwdPath ,  ' defaultPath ', defaultPath)
 
 let cwdAppConfig, defAppConfig
 
